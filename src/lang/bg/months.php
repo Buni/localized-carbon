475d71f0d3c984430ee76cf52d68feb21a6dd64b
Buni <xkyrchokov@gmail.com>
2017-01-07 01:49:15 +0200

    bg months

---
+++ src/lang/bg/months.php
@@ -0,0 +1,16 @@
+<?php
+
+return array(
+    "january" => "януари",
+    "february" => "февруари",
+    "march" => "март",
+    "april" => "април",
+    "may" => "май",
+    "june" => "юни",
+    "july" => "юли",
+    "august" => "август",
+    "september" => "септември",
+    "october" => "октомври",
+    "november" => "ноември",
+    "december" => "декември",
+);
